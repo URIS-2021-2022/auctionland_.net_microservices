356a3d11aef17c6b4c0abb1680858ed4ac2bd588
damjanivetic <damjanivetic@uns.ac.rs>
2022-02-23 17:21:49 +0100

    Konacno

--- Komisija_Agregat/Entities/KomisijaContext.cs
+++ Komisija_Agregat/Entities/KomisijaContext.cs
@@ -31,32 +31,7 @@ namespace Komisija_Agregat.Entities
         {
             optionsBuilder.UseSqlServer(configuration.GetConnectionString("KomisijaDB"));
         }
-        /*
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<Predsednik>().HasData(new
-            {
-                PredsednikId = Guid.Parse("6a411c13-a195-48f7-8dbd-67596c3974c0"),
-                ImePredsednika = "Petar",
-                PrezimePredsednika = "Markovic",
-                EmailPredsednika = "markuza@mail.com"
-            });
-            builder.Entity<ClanKomisije>().HasData(new
-            {
-                ClanId = Guid.Parse("1c7ea607-8ddb-493a-87fa-4bf5893e965b"),
-                ImeClana = "Nenad",
-                PrezimeClana = "Jeckovic",
-                EmailClana = "jocko@mail.com"
-            });
-            builder.Entity<Komisija>().HasData(new
-            {
-                KomisijaId = Guid.Parse("1c7ea607-8ddb-493a-87fa-4bf5893e965b"),
-                Predsednik = "sd",
-                Clanovi = "sdd"
-            }); 
-        }
-
-        */
+       
 
     }
 }
